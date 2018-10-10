114fb14eace583a978c3cbdddfc4a6df55cb3a40
mijikim <miji.kim926@gmail.com>
2018-10-10 12:39:22 -0400

    Updates

---
+++ precache-manifest.957f57905e8920d4efc70a3d5be88258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b02aa437bd9a314569af",
+    "url": "/bbi_client/static/js/runtime~main.b02aa437.js"
+  },
+  {
+    "revision": "62722e513bcfd4febbbe",
+    "url": "/bbi_client/static/js/main.62722e51.chunk.js"
+  },
+  {
+    "revision": "4f8696b018c41f8bc562",
+    "url": "/bbi_client/static/js/1.4f8696b0.chunk.js"
+  },
+  {
+    "revision": "62722e513bcfd4febbbe",
+    "url": "/bbi_client/static/css/main.8245fd2a.chunk.css"
+  },
+  {
+    "revision": "f097f917a794ce99c950837ce682f889",
+    "url": "/bbi_client/index.html"
+  }
+];
